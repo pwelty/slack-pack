612dd6146ba34bdf74fd734949ff8fbff1d01290
Paul Welty <paul.welty@sparksgrove.com>
2016-07-21 12:55:50 -0400

    new config file

--- web/config.php
+++ web/config.php
@@ -6,15 +6,16 @@ $channels = array();
 
 // $channels[] = 'general';
 // $map['sg-general']=$channels;
-//
-// $channels[] = 'tech_digital';
-// $map['sg-tech']=$channels;
+
+$channels[] = 'tech_digital';
+$map['sg-tech']=$channels;
 
 $channels[] = 'tech_digital_dev';
 $map['sg-dev']=$channels;
 
 // $channels[] = 'ai';
 // $map['sg-ai']=$channels;
+
 // $channels[] = 'sparks_grove_creative';
 // $map['sg-creative']=$channels;
 
