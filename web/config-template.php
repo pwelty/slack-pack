0079eb617f183659ab18636da84fa1ba9320e864
Paul Welty <paul.welty@sparksgrove.com>
2016-07-21 15:19:14 -0400

    updated config template

--- web/config-template.php
+++ web/config-template.php
@@ -10,10 +10,5 @@ $map['POCKET_TAG_NAME']=$channels;
 $channels[] = 'OTHER_DESTINATION_SLACK_CHANNEL_NAME';
 $map['OTHER POCKET_TAG_NAME']=$channels;
 
-// $channels[] = 'ai';
-// $map['sg-ai']=$channels;
-
-// $channels[] = 'sparks_grove_creative';
-// $map['sg-creative']=$channels;
 
 ?>
