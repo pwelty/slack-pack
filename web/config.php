41df0970f45842dedd9d0f0adc3f7c2a1b788fd6
Paul Welty <paul.welty@sparksgrove.com>
2016-07-21 08:54:09 -0400

    updated channels config

--- web/config.php
+++ web/config.php
@@ -3,10 +3,16 @@
 // CREATE (POCKET) TAG TO (SLACK) CHANNEL MAP
 $map = array();
 $channels = array();
-$channels[] = 'general';
-$map['sg-general']=$channels;
+
+// $channels[] = 'general';
+// $map['sg-general']=$channels;
+//
 // $channels[] = 'tech_digital';
 // $map['sg-tech']=$channels;
+
+$channels[] = 'tech_digital_dev';
+$map['sg-dev']=$channels;
+
 // $channels[] = 'ai';
 // $map['sg-ai']=$channels;
 // $channels[] = 'sparks_grove_creative';
