cc132e9703845c1ab2078f8f65cbcc535e5f9a35
Paul Welty <paul.welty@sparksgrove.com>
2016-07-26 15:15:10 -0400

    refactor for fun and profit

--- web/helpers.php
+++ web/helpers.php
@@ -1,32 +1,28 @@
 <?php
 
-function r($a,$l='') {
-  if (is_array($a) || is_object($a)) {
-    if ($l) {
-      echo "<p><b>".$l."=</b></p><pre>".print_r($a,TRUE)."</pre>";
-    } else {
-      echo "<pre>".print_r($a,TRUE)."</pre>";
-    }
-    return "<pre>".print_r($a,TRUE)."</pre>";
+function r($anArray,$aLabel='') {
+  $out = '';
+  if ($aLabel) {
+    $someText = "<p><b>".$l."=</b></p>";
+    echo $someText;
+    $out .= $someText;
+  }
+  if (is_array($anArray) || is_object($a)) {
+      $someText = "<p><pre>".print_r($anArray,TRUE)."</pre></p>";
   } else {
-    if ($l) {
-      echo "<p><b>".$l."=</b></p><p>".$a."</p>";
-    } else {
-      echo "<p><b>".$a."</b></p>";
-    }
-    return "<p><b>".$a."</b></p>";
+    $someText = "<p><b>".$anArray."</b></p>";
   }
 }
 
 function mailIt($html,$fromEmail,$toEmail) {
   $from = new SendGrid\Email(null,$fromEmail);
   $subject = "Slack Pack notification";
-  $to = new SendGrid\Email(null,$toEmail);
+  $toEmailThing = new SendGrid\Email(null,$toEmail);
   $content = new SendGrid\Content("text/html", "<html>".$html."</html>");
-  $mail = new SendGrid\Mail($from, $subject, $to, $content);
+  $mail = new SendGrid\Mail($from, $subject, $toEmailThing, $content);
   $apiKey = getenv('SENDGRID_API_KEY');
-  $sg = new \SendGrid($apiKey);
-  $response = $sg->client->mail()->send()->post($mail);
+  $sendGridGoGo = new \SendGrid($apiKey);
+  $response = $sendGridGoGo->client->mail()->send()->post($mail);
   echo $response->statusCode();
   echo $response->headers();
   echo $response->body();
