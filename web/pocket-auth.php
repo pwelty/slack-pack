7cd1e98062cb2603e4d1ee32bd557e3f73a878e8
Paul Welty <paul@synaxisworks.com>
2016-10-09 11:35:09 -0400

    pocket stuff

--- web/pocket-auth.php
+++ web/pocket-auth.php
@@ -2,9 +2,10 @@
 
 require_once 'helpers.php';
 require_once 'pocket.class.php';
+require_once 'config.php';
 
 if (isset($config->pocket_consumer_key)) {
-    $key = $config->pocket_consumer_key;
+  $key = $config->pocket_consumer_key;
 } else {
   $key = getenv('POCKET_CONSUMER_KEY');
 }
