bb6c0e3a8145d593e43f5d5ffa5c0f6d67575400
Paul Welty <paul@synaxisworks.com>
2016-10-09 13:30:11 -0400

    updated config template

--- web/config-template.php
+++ web/config-template.php
@@ -1,10 +1,24 @@
 <?php
 
-// CREATE (POCKET) TAG TO (SLACK) CHANNEL MAP
-$map = array();
+$config = new stdClass;
 
-$map['POCKET_TAG_NAME']='DESTINATION_SLACK_CHANNEL_NAME';
+$config->map = array();
+$config->map['POCKET_TAG_NAME']='SLACK_CHANNEL_NAME'; // NOTE: don't need "#" in front
+//$map['OTHER POCKET_TAG_NAME']='OTHER_DESTINATION_SLACK_CHANNEL_NAME';
 
-$map['OTHER POCKET_TAG_NAME']='OTHER_DESTINATION_SLACK_CHANNEL_NAME';
+$config->suffix = "-posted"; // not used right now but maybe
+
+$config->simulate_channel = ''; // where you want your simulate slack messages to go. Use "@username" to send to yourself.
+
+$config->slack_token = ''; // Could put this in ENV vars, too
+
+$config->pocket_consumer_key = ''; // Could put this in ENV vars, too
+$config->pocket_access_token = ''; // Could put this in ENV vars, too
+
+$config->heroku_subdomain = ''; // really needed??
+
+$config->sendgrid_api_key = ''; // Could put this in ENV vars, too
+
+$config->to_email = ''; // where email reports are sent
 
 ?>
