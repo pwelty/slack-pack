b1831b9c0bec8c7d5a122c5141110b631ffaee85
Paul Welty <paul@synaxisworks.com>
2018-05-20 13:18:51 -0400

    tweaks based on sonarlint

--- web/index.php
+++ web/index.php
@@ -11,7 +11,6 @@ $log->pushHandler(new StreamHandler('php://stderr', Logger::INFO));
 $log->addInfo('Starting app',array('starting'=>'whatevs'),'extra');
 
 require_once 'helpers.php';
-// require_once 'pocket.class.php';
 require_once 'slack.class.php';
 
 
@@ -24,7 +23,6 @@ if (file_exists('config.php')) {
 }
 
 // GET ENV VARS. FOR LOCAL USING .HTACCESS FILE
-
 if (isset($config->pocket_suffix)) {
   $pocketSuffix = $config->pocket_suffix;
 } else {
@@ -139,20 +137,11 @@ if (!$simulate && ($dayOfWeek==0 || $dayOfWeek==6) && $skipWeekend) {
 $log->addInfo("Seems ok. Starting!");
 
 // CREATE COMM OBJECTS
-// $pocket = new Pocket($pocketConsumerKey,$pocketAccessToken,'action',$simulate,$pocketSuffix);
 $pocket = new Pocket($pocketConsumerKey,$pocketAccessToken,$simulate);
-
-// $result = $pocket->getPosts('sg-general-posted','1','oldest','complete');
-// $list = $result->list;
-// print_r($list);
-// exit;
-
-
 $slack  = new Slack($slackToken,$simulateChannel,$simulate);
 
-
 // START THE EMAIL REPORT VAR
-$out = r($simulate,'simulate');
+$out  = r($simulate,'simulate');
 $out .= r($specifiedTag,'specified tag');
 $out .= r($_SERVER['SERVER_NAME'],'server');
 $out .= r($channelMap,'map');
@@ -161,14 +150,11 @@ $out .= r($channelMap,'map');
 $thePosts = array();
 foreach ($channelMap as $tag=>$channel) {
 
-  if ($specifiedTag!='') {
-    if ($tag!=$specifiedTag) {
-      $out .= "<p>Skipped ".$tag."</p>";
-      continue;
-    }
+  if ($specifiedTag!='' && $tag!=$specifiedTag) {
+    $out .= "<p>Skipped ".$tag."</p>";
+    continue;
   }
-  // $out .= r($tag);
-  echo "<p>Connecting to Pocket, looking for ".$tag."</p>";
+  echo "<p>Connecting to Pocket, looking for posts with tag = ".$tag."</p>";
 
   $posts = $pocket->getPosts($tag,0);
 
@@ -181,16 +167,12 @@ foreach ($channelMap as $tag=>$channel) {
   }
 }
 
-function cmp($a, $b) {
-  return $a->time_added < $b->time_added;
+function cmp($aChoice, $bChoice) {
+  return $aChoice->time_added < $bChoice->time_added;
 }
 usort($thePosts, "cmp");
 
-
-
 if (!empty($thePosts)) {
-
-//  $out .= r($thePosts);
 
   $numberPosts = count($thePosts);
   $out .= "<p>".$numberPosts . " posts</p>";
@@ -199,17 +181,11 @@ if (!empty($thePosts)) {
 		$out .= "<p>".$aPost->resolved_title . "(".date("m/d/Y",$aPost->time_added).") [".$aPost->channel."]</p>";
 	}
 
-//   $thePostIndex = rand(0,$numberPosts-1);
   $thePostIndex = 0;
   $thePost = $thePosts[$thePostIndex];
   $out .= r($thePost);
   $out .= r(date("r",$thePost->time_added));
   postAPost($thePost);
-  // foreach ($thePosts as $aPost) {
-  //
-  //   $postAPost($aPost);
-  //
-  // } // THEPOSTS
 }
 
 
@@ -220,7 +196,7 @@ echo "<p>emails going to ".$toEmail."</p>";
 mailIt($out,$toEmail,$toEmail,$apiKey);
 
 function postAPost($aPost) {
-  global $slack,$pocket,$out;
+  global $slack,$pocket,$out,$pocketSuffix;
   // BUILD THE POST TEXT
   $excerpt = $aPost->excerpt;
   $url = $aPost->resolved_url;
@@ -230,9 +206,7 @@ function postAPost($aPost) {
   }
   $title = $aPost->resolved_title;
   $id = $aPost->item_id;
-  $text = $url;
-  // $text = "*".$title."*\n".$excerpt."\n<".$url."> ";
-  // r($excerpt,"excerpt");
+  $text = $excerpt ." ".$url;
 
   // POST
   $ttt = "<p>Posting ".$title." (".$text.") TO ".$aPost->channel." (from ".$aPost->tag.")</p>";
@@ -242,12 +216,10 @@ function postAPost($aPost) {
   echo "<p>Connecting to Slack to post in the ".$aPost->channel." channel</p>";
   $response = $slack->postTextToChannel($text,$aPost->channel);
   echo "<p>Back from Slack...".$response."</p>";
-  // $out .= r($response,$channel);
   echo "<p>Untagging...</p>";
   echo "<p>To be specific ... " . $id . " - " . $aPost->tag.$pocketSuffix . "</p>";
   $response = $pocket->tagPost($id,$aPost->tag.$pocketSuffix);
   $response = $pocket->untagPost($id,$aPost->tag);
-  // $out .= r($response,'UNTAGGED');
   echo "<p>Completed slack posting for this tag.</p>";
 
 }
